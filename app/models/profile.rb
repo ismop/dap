a05e3a4efa92402fa8ed1a1236e98aeb60527af5
Bartosz Wilk <b.wilk@cyfronet.pl>
2014-10-24 19:29:27 +0200

    Fixed mistake

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,6 @@
 class Profile < ActiveRecord::Base
 
   validates_presence_of :profile_type
-  validates :emergency_level, inclusion: {in: ["none", "heightened", "severe"]}
   validates :threat_level, inclusion: {in: ["none", "heightened", "severe"]}
 
   has_many :sensors, dependent: :destroy
