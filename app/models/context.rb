17ee3329b50923e7f48686cc89fdb3144e80706a
Bartosz Wilk <b.wilk@cyfronet.pl>
2015-07-14 13:02:57 +0200

    Added third context type

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,6 +1,6 @@
 # This class is used to group timelines and scenarios in order to switch between different system perspectives.
 class Context < ActiveRecord::Base
-  validates :context_type, inclusion: {in: ["measurements", "tests"]}
+  validates :context_type, inclusion: {in: ["measurements", "tests", "other"]}
   validates_presence_of :name
   has_many :timelines, dependent: :destroy
   has_many :scenarios, dependent: :destroy
