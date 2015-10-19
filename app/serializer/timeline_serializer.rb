fd787fa3023b05b5ad1f577f853aac0841504e8b
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2015-10-19 15:08:36 +0200

    Embed scenario_id in timeline JSON

--- app/serializer/timeline_serializer.rb
+++ app/serializer/timeline_serializer.rb
@@ -1,3 +1,3 @@
 class TimelineSerializer < ActiveModel::Serializer
-  attributes :id, :label, :sensor_id, :context_id, :parameter_id, :experiment_id
+  attributes :id, :label, :sensor_id, :context_id, :parameter_id, :experiment_id, :scenario_id
 end
