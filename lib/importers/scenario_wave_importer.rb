5ab4a1ca2e4d6667da7c5bda1878b586bfab0316
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-11-04 18:52:02 +0100

    Load experiment water wave from CSV file

---
+++ lib/importers/scenario_wave_importer.rb
@@ -0,0 +1,46 @@
+require 'csv'
+
+module Importers
+  class ScenarioWaveImporter
+    def initialize(context, experiment, water_wave_parameter, csv_path)
+      @context = context
+      @experiment = experiment
+      @water_wave_parameter = water_wave_parameter
+      @csv_path = csv_path
+    end
+
+    def import
+      @measurements = [new_measurement(0, 0)]
+
+      puts 'Loading water wave...'
+      ActiveRecord::Base.transaction do
+        CSV.foreach(@csv_path, col_sep: ';') do |row|
+          @measurements << new_measurement(row[0].to_i, row[1])
+        end
+        puts 'Importing water wave...'
+        Measurement.import(@measurements)
+        puts 'Updating experiment start date...'
+        update_start_date(@measurements.last.timestamp.to_i)
+      end
+    end
+
+    private
+
+    def update_start_date(experiment_length)
+      start_date = @experiment.end_date - experiment_length.seconds
+      @experiment.update_attribute(:start_date, start_date)
+    end
+
+    def new_measurement(timestamp, value)
+      Measurement.new(timeline: timeline,
+                      timestamp: Time.at(timestamp),
+                      value: value)
+    end
+
+    def timeline
+      @timeline ||= Timeline.find_or_create_by(parameter: @water_wave_parameter,
+                                               experiment: @experiment,
+                                               context: @context)
+    end
+  end
+end
--- lib/tasks/scenarios_load.rake
+++ lib/tasks/scenarios_load.rake
@@ -1,15 +1,25 @@
-require 'importers/scenarios_importer'
-
 namespace :scenarios do
   desc 'Load experiment scenarios from CSV file'
   task :load, [:levee_id,
                :context_id,
                :scenarios_file_path] => :environment do |t, args|
+    require 'importers/scenarios_importer'
     if_scenario_path_exist!(args[:scenarios_file_path]) do |path|
       Importers::ScenariosImporter.
         new(levee!(args[:levee_id]),
             context!(args[:context_id]), path).import
       puts "Import was finished without any errors"
+    end
+  end
+
+  task :load_water_wave, [:context_id, :experiment_id,
+                          :wave_file_path] => :environment do |t, args|
+    require 'importers/scenario_wave_importer'
+    if_scenario_path_exist!(args[:wave_file_path]) do |path|
+      Importers::ScenarioWaveImporter.
+        new(context!(args[:context_id]),
+            experiment!(args[:experiment_id]),
+            wather_wave_parameter, path).import
     end
   end
 
@@ -27,6 +37,17 @@ namespace :scenarios do
   rescue
     puts "ERROR: Context with id eq #{id} is not found"
     exit 1
+  end
+
+  def experiment!(id)
+    Experiment.find(id)
+  rescue
+    puts "ERROR: Experiment with id eq #{id} is not found"
+    exit 1
+  end
+
+  def wather_wave_parameter
+    Parameter.find_by!(custom_id: 'wave_height')
   end
 
   def if_scenario_path_exist!(path)
