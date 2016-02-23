383a7e32018347b24fd2eadf6a25224af0227cfa
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2016-02-23 16:41:55 +0100

    Make sure to include latest measurement for each timeline when the quantity flag is used.

--- spec/requests/api/measurements_spec.rb
+++ spec/requests/api/measurements_spec.rb
@@ -207,7 +207,7 @@ describe Api::V1::MeasurementsController do
       it 'returns exactly the requested number of measurements when the quantity flag is used' do
         time = Time.now
         ms1, ms2, ms3 = [], [], []
-        for i in 1..1000 do
+        for i in 1..1500 do
           ms1 << create(:measurement, timeline: t1, timestamp: time+i.seconds)
           ms2 << create(:measurement, timeline: t2, timestamp: time+(i*5).seconds)
           ms3 << create(:measurement, timeline: t3, timestamp: time+(i*100).seconds)
@@ -218,6 +218,12 @@ describe Api::V1::MeasurementsController do
         expect(ms_response.select{|m| m['timeline_id'] == t1.id}.length).to eq 27
         expect(ms_response.select{|m| m['timeline_id'] == t2.id}.length).to eq 27
         expect(ms_response.select{|m| m['timeline_id'] == t3.id}.length).to eq 27
+
+        # Also check whether the final measurement is returned for each timeline
+        # This is done w/o a separate spec for performance reasons (populating the DB takes a long time)
+        expect(ms_response.collect{|m| m['id']}).to include t3.measurements.last.id
+        expect(ms_response.collect{|m| m['id']}).to include t2.measurements.last.id
+        expect(ms_response.collect{|m| m['id']}).to include t1.measurements.last.id
       end
 
       it 'returns an empty table when no results are found' do
