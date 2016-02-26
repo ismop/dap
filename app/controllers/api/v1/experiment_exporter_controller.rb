f2b7504afddeadf557f2f6bbb623443d2fd4a91f
Bartosz Wilk <b.wilk@cyfronet.pl>
2016-02-26 12:55:38 +0100

    Streaming introduced in experiment export controller

--- app/controllers/api/v1/experiment_exporter_controller.rb
+++ app/controllers/api/v1/experiment_exporter_controller.rb
@@ -8,10 +8,33 @@ module Api
         authorize! :read, :experiment_exporter
         experiment_id = params[:id]
         exporter = Exporters::ExperimentExporter.new(experiment_id)
-        file = exporter.export
-        send_file(file,
-                  :filename => "experiment.csv",
-                  :type => "text/csv")
+        writer = StreamWriter.new(response, "experiment.csv")
+        exporter.export2(writer)
+        # send_file(file,
+        #           :filename => "experiment.csv",
+        #           :type => "text/csv")
+      ensure
+        response.stream.close
+      end
+
+      class StreamWriter
+
+        def initialize(response, filename)
+          @response = response
+          @filename = filename
+          @first_write = true
+        end
+
+        def write(data)
+          if @first_write
+            @response.headers['Content-Type'] = 'Application/octet-stream'
+            @response.headers['Content-Disposition'] = 'attachment; filename="' + @filename + '"'
+            @response.headers['X-Accel-Buffering'] = 'no'
+            @first_read = false
+          end
+          @response.stream.write data
+        end
+
       end
 
     end
--- lib/exporters/experiment_exporter.rb
+++ lib/exporters/experiment_exporter.rb
@@ -20,7 +20,35 @@ module Exporters
           .eager_load(timeline: { parameter: :device })
           .where("timelines.context_id" => context.id)
           .where("devices.id" => devices)
+          .select('measurements.timeline_id, timelines.parameter_id, timelines.context_id, parameters.device_id, '\
+                'measurements.value, measurements.m_timestamp, '\
+                'parameters.custom_id, '\
+                'devices.placement')
           .order(:m_timestamp)
+    end
+
+    def export2(writer)
+      id = @experiment_id
+      experiment = Experiment.find(id); return [] if experiment.blank?
+      levee = experiment.levee; return [] if levee.blank?
+      context = Context.find_by(context_type: 'measurements'); return [] if context.blank?
+      devices = levee.devices.ids; return [] if devices.blank?
+      devices.each_slice(4) do |devices_slice|
+        measurements = Measurement
+            .after_date(experiment.start_date, true)
+            .before_date(experiment.end_date, true)
+            .eager_load(timeline: { parameter: :device })
+            .where("timelines.context_id" => context.id)
+            .where("devices.id" => devices_slice)
+            .select('measurements.timeline_id, timelines.parameter_id, timelines.context_id, parameters.device_id, '\
+                'measurements.value, measurements.m_timestamp, '\
+                'parameters.custom_id, '\
+                'devices.placement')
+            .order(:m_timestamp)
+          measurements.each do |m|
+            writer.write(serializer.serialize(m))
+          end
+      end
     end
 
     def serializer
@@ -38,7 +66,7 @@ module Exporters
           y = xyz.y
           z = xyz.z
         end
-        name = m.timeline.parameter.measurement_type.name
+        name = m.timeline.parameter_id #.measurement_type.name
         val = '%.8f' % m.value
         [time, x, y, z, name, val]
 
