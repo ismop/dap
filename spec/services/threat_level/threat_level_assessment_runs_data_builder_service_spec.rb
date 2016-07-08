60e06ce1bb6fb255544e18e88efc2965fc2f3af7
tomek <tomek.bartynski@gmail.com>
2016-07-08 13:33:45 +0200

    svc building run data for given profile
    https://github.com/ismop/dap/issues/115

---
+++ spec/services/threat_level/threat_level_assessment_runs_data_builder_service_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe ThreatLevel::AssessmentRunsDataBuilderService do
+  let!(:profile) { create(:profile, threat_assessments: []) }
+
+  context 'no runs associated with profile' do
+    it 'returns empty array' do
+      expect(ThreatLevel::AssessmentRunsDataBuilderService.get(profile)).to eq []
+    end
+  end
+
+  context 'runs associated with profile' do
+    let!(:run) { create(:threat_assessment_run) }
+    let!(:assessment) do
+      create(
+        :threat_assessment,
+        threat_assessment_run: run,
+        profiles: [profile]
+      )
+    end
+
+    it 'returns array with run data' do
+      # reload are required to make associations visible
+      profile.reload
+      run.reload
+
+      expect(ThreatLevel::AssessmentRunsDataBuilderService.get(profile)).
+        to eq runs_data_array
+    end
+  end
+end
+
+private
+def runs_data_array
+  [
+   ThreatLevel::AssessmentRunStatusDataBuilderService.get(run)
+  ]
+end
