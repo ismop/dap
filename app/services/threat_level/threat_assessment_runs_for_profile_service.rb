e4d3a125f05968efd00efde7d630950ee89006d6
tomek <tomek.bartynski@gmail.com>
2016-09-05 12:34:42 +0200

    return only 5 last runs
    https://github.com/ismop/dap/issues/151

--- app/services/threat_level/threat_assessment_runs_for_profile_service.rb
+++ app/services/threat_level/threat_assessment_runs_for_profile_service.rb
@@ -1,8 +1,8 @@
 module ThreatLevel
   class ThreatAssessmentRunsForProfileService
-    def self.get(profile)
+    def self.get(profile_id)
       ThreatAssessmentRun.joins(threat_assessments: :profiles).
-          where("profiles.id = #{profile.id}")
+          where("profiles.id = #{profile_id}").order(created_at: :desc).limit(5)
     end
   end
 end
