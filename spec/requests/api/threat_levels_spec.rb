b79438a5e48ff53ae62d8ad7593a59472ebfd1a7
tomek <tomek.bartynski@gmail.com>
2016-09-05 12:35:35 +0200

    refactored building response
    https://github.com/ismop/dap/issues/151
    
    Changed DB query and building response in order to improve performance.

--- spec/requests/api/threat_levels_spec.rb
+++ spec/requests/api/threat_levels_spec.rb
@@ -36,16 +36,19 @@ describe Api::V1::ThreatLevelsController do
     context 'profile present in DB' do
       let!(:profile) { create(:profile) }
 
-      it 'returns profile without data' do
+      it 'returns threat_levels with empty array value' do
         get api("/threat_levels", user)
-        expect(json_response).to eq(threat_levels_with_profile_only)
+        expect(json_response).to eq({'threat_levels' => []})
       end
 
       context 'threat level data present in DB' do
         let!(:threat_assessment) { create(:threat_assessment, profiles: [profile]) }
         let!(:scenario1) { create(:scenario, threat_level: 2) }
         let!(:scenario2) { create(:scenario, threat_level: 1) }
-        let!(:result1) { create(:result, threat_assessment: threat_assessment, similarity: 0.8, scenario: scenario1) }
+        let!(:result1) do
+          create(:result,
+                 threat_assessment: threat_assessment, similarity: 0.8, scenario: scenario1)
+        end
         let!(:result2) { create(:result, threat_assessment: threat_assessment, similarity: 0.4, scenario: scenario2) }
 
         it 'returns threat levels data' do
@@ -55,8 +58,11 @@ describe Api::V1::ThreatLevelsController do
 
         context '6 assessments exist in DB' do
           before do
-            5.times { create(:threat_assessment, profiles: [profile]) }
+            5.times do
+              create(:result, threat_assessment: create(:threat_assessment, profiles: [profile]))
+            end
           end
+
           it 'returns 5 latest assesments by default' do
             get api("/threat_levels", user)
             expect(contains_5_latest_assessment(assessments_from_response)).to be_truthy
@@ -97,44 +103,40 @@ def expected_assessment_runs_data
   ]
 end
 
-def threat_levels_with_profile_only
-  {
+def threat_levels_with_results
+  threat_levels = {
     'threat_levels' => [
       {
         'profile_id' => profile.id,
-        'threat_assessments' => [],
+        'threat_assessments' => [
+          {
+            'date' => threat_assessment.created_at.as_json,
+            'status' => threat_assessment.status,
+            'scenarios' => [
+              {
+                'similarity' => result1.similarity,
+                'threat_level' => 2,
+                'scenario_id' => result1.scenario_id,
+                'offset' => result1.offset,
+                'name' => result1.scenario.name,
+                'description' => result1.scenario.description
+              },
+              {
+                'similarity' => result2.similarity,
+                'threat_level' => 1,
+                'scenario_id' => result2.scenario_id,
+                'offset' => result2.offset,
+                'name' => result2.scenario.name,
+                'description' => result2.scenario.description
+              }
+            ]
+          }
+        ],
         'threat_level_assessment_runs' => []
       }
     ]
   }
-end
 
-def threat_levels_with_results
-  threat_levels = threat_levels_with_profile_only
-  threat_levels['threat_levels'][0]['threat_assessments'] = [
-    {
-      'date' => threat_assessment.created_at.as_json,
-      'status' => threat_assessment.status,
-      'scenarios' => [
-        {
-          'similarity' => result1.similarity,
-          'threat_level' => 2,
-          'scenario_id' => result1.scenario_id,
-          'offset' => result1.offset,
-          'name' => result1.scenario.name,
-          'description' => result1.scenario.description
-        },
-        {
-          'similarity' => result2.similarity,
-          'threat_level' => 1,
-          'scenario_id' => result2.scenario_id,
-          'offset' => result2.offset,
-          'name' => result2.scenario.name,
-          'description' => result2.scenario.description
-        }
-      ]
-    }
-  ]
   threat_assessment_run = threat_assessment.threat_assessment_run
   threat_levels['threat_levels'][0]['threat_level_assessment_runs'] =
     [
