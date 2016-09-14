a35ba7c38277eb3c47ea98df71d66b922a3c9272
tomek <tomek.bartynski@gmail.com>
2016-09-14 14:38:46 +0200

    optimized getting threat assessment run data

--- app/services/threat_level/threat_assessment_runs_for_profile_service.rb
+++ app/services/threat_level/threat_assessment_runs_for_profile_service.rb
@@ -1,7 +1,8 @@
 module ThreatLevel
   class ThreatAssessmentRunsForProfileService
     def self.get(profile_id)
-      ThreatAssessmentRun.joins(threat_assessments: :profiles).
+      ThreatAssessmentRun.includes(threat_assessments: :results).
+        joins(threat_assessments: :profiles).
           where("profiles.id = #{profile_id}").order(created_at: :desc).limit(5)
     end
   end
